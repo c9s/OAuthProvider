0c0ab60df20fe601e7d78d3396d6d4f8ff995dff
c9s <cornelius.howl@gmail.com>
2012-01-19 06:20:09 +0800

    Add Yahoo OAuth

---
+++ src/OAuthProvider/OAuth_1a/OAuthYahoo.php
@@ -0,0 +1,24 @@
+<?php
+namespace OAuthProvider\OAuth_1a;
+
+class OAuthYahoo extends AbstractProvider
+{
+    const version = '1.0a';
+    const host = 'api.login.yahoo.com';
+
+    public function getRequestTokenUrl() { return 'https://' . self::host . '/oauth/v2/get_request_token'; }
+
+    public function getRequestTokenMethod() { return 'POST'; }
+
+    public function getAccessTokenUrl() { return 'https://' . self::host . '/oauth/v2/get_token'; }
+
+    public function getAccessTokenMethod() { return 'POST'; }
+
+    public function getAuthorizeUrl() { return 'https://' . self::host . '/oauth/v2/request_auth'; }
+
+}
+
+
+
+
+
