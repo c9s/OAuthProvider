b1dbcdd67aeab476f5d664e1319fca689d2d348d
c9s <cornelius.howl@gmail.com>
2012-01-19 06:03:14 +0800

    Update GithuB provider class

--- src/OAuthProvider/OAuth_2/OAuthGitHub.php
+++ src/OAuthProvider/OAuth_2/OAuthGitHub.php
@@ -3,21 +3,6 @@ namespace OAuthProvider\OAuth_2;
 
 class OAuthGitHub extends AbstractProvider
 {
-
-    /*
-    qw(client_id client_secret authorize_url access_token_url);
-
-	version   => 2,
-	authorize_url    => 'https://github.com/login/oauth/authorize',
-	access_token_url => 'https://github.com/login/oauth/access_token',
-	scope            => 'user,public_repo'
-
-	authorize_url    => 'https://accounts.google.com/o/oauth2/auth',
-	access_token_url => 'https://accounts.google.com/o/oauth2/token',
-	response_type    => 'code',
-	grant_type       => 'authorization_code',
-	request_method   => 'POST',
-    */
     const host = 'github.com';
 
     public function getAuthorizeUrl() {
@@ -35,4 +20,5 @@ class OAuthGitHub extends AbstractProvider
     public function getResponseType() { 
         return 'token';
     }
+
 }
