9ea1cac2b8ad8dfc2b96bc67ad874ee7cfc0dd00
c9s <cornelius.howl@gmail.com>
2012-01-19 04:17:26 +0800

    Add OAuthProvider create method

---
+++ src/OAuthProvider/OAuthProvider.php
@@ -0,0 +1,38 @@
+<?php
+namespace OAuthProvider;
+use ReflectionClass;
+
+class OAuthProvider 
+{
+
+    static $providers = array(
+        'twitter' => 'OAuthProvider\OAuth_1a\OAuthTwitter',
+        'plurk'   => 'OAuthProvider\OAuth_1a\OAuthPlurk',
+        'facebook'   => 'OAuthProvider\OAuth_2\OAuthFacebook',
+        'github'     => 'OAuthProvider\OAuth_2\OAuthGitHub',
+        'google'     => 'OAuthProvider\OAuth_2\OAuthGoogle',
+    );
+
+    static function getClass($providerName)
+    {
+        return self::providers[ $providerName ];
+    }
+
+
+    /**
+     * static method for creating provider object
+     *
+     * OAuthProvider::create('twitter', array( 
+     *    'consumer_key' => ...,
+     *    'consumer_secret' => ...,
+     * ));
+     *
+     */
+    static function create($providerName, $args = array())
+    {
+        $class = self::providers[ $providerName ];
+        $rc = new ReflectionClass( $class );
+        return $rc->newInstanceArgs( $args );
+    }
+
+}
